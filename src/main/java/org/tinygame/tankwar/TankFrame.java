f90f14da6a2287e30515ff825a2f3cb6b2f523c0
huanyu.li <cn.hylee@gmail.com>
2026-03-06 00:20:50 +0800

    feat: add keyboard control to move rectangle

--- src/main/java/org/tinygame/tankwar/TankFrame.java
+++ src/main/java/org/tinygame/tankwar/TankFrame.java
@@ -1,6 +1,8 @@
 package org.tinygame.tankwar;
 
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -8,6 +10,7 @@ import java.awt.event.WindowEvent;
  * 坦克大战游戏主窗口类
  */
 public class TankFrame extends Frame {
+    private int x = 200, y = 200;
 
     public TankFrame() {
         this.setTitle("Tank War");
@@ -21,10 +24,28 @@ public class TankFrame extends Frame {
                 System.exit(0);
             }
         });
+
+        this.addKeyListener(new KeyListener());
+    }
+
+    class KeyListener extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            x += 20;
+            y += 20;
+            repaint();
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            x -= 10;
+            y -= 10;
+            repaint();
+        }
     }
 
     @Override
     public void paint(Graphics g) {
-        g.fillRect(200, 200, 50, 50);
+        g.fillRect(x, y, 50, 50);
     }
 }
