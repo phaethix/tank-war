7c007e621d255db78e81b271c01db314b1037523
huanyu.li <cn.hylee@gmail.com>
2026-03-08 01:06:29 +0800

    feat: clean up bullets when out of screen

--- src/main/java/org/tinygame/tankwar/Bullet.java
+++ src/main/java/org/tinygame/tankwar/Bullet.java
@@ -6,19 +6,25 @@ import java.awt.*;
  * 子弹类
  */
 public class Bullet {
-    private static final int SPEED = 1;
+    private static final int SPEED = 10;
     private static final int WIDTH = 30, HEIGHT = 30;
 
     private int x, y;
     private final Dir dir;
+    private boolean inactive;
+    private TankFrame frame;
 
-    public Bullet(int x, int y, Dir dir) {
+    public Bullet(int x, int y, Dir dir, TankFrame frame) {
         this.x = x;
         this.y = y;
         this.dir = dir;
+        this.frame = frame;
     }
 
     public void paint(Graphics g) {
+        if (inactive) {
+            frame.bullets.remove(this);
+        }
         Color c = g.getColor();
         g.setColor(Color.RED);
         g.fillOval(x, y, WIDTH, HEIGHT);
@@ -35,6 +41,10 @@ public class Bullet {
             case DOWN -> y += SPEED;
             default -> {
             }
+        }
+
+        if (x < 0 || y < 0 || x > TankFrame.GAME_WIDTH || y > TankFrame.GAME_HEIGHT) {
+            inactive = true;
         }
     }
 }
