1a88c11f69edd716f5208de84fde84220999e1dd
huanyu.li <cn.hylee@gmail.com>
2026-03-28 09:14:50 +0800

    chore: remove macOS build from release workflow and add local packaging script

---
+++ package-macos.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+mvn clean package
+
+jpackage \
+  --name tank-war \
+  --input target \
+  --main-jar tank-war-1.0-SNAPSHOT.jar \
+  --main-class org.tinygame.tankwar.Main \
+  --type app-image \
+  --java-options "--enable-preview" \
+  --verbose
+
+sudo xattr -cr tank-war.app
+
+zip -r TankWar-macOS.zip tank-war.app
+
+rm -rf tank-war.app
